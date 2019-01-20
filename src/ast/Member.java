5e93307ed0ea8825b58010bd217a7e325853864a
João Vitor de Sá <jvsms13@gmail.com>
2019-01-20 09:50:43 -0200

    2 lexer error files enconding converted; New ast.Member class; Filling SymbolTable

---
+++ src/ast/Member.java
@@ -0,0 +1,11 @@
+package ast;
+
+/*
+Member::= FieldDec | MethodDec
+FieldDec::= “var” Type IdList [ “;” ]
+MethodDec::= “func” IdColon FormalParamDec [ “->” Type ]
+    “{” StatementList “}” |
+    “func” Id [ “->” Type ] “{” StatementList “}”
+* */
+public class Member {
+}
--- src/comp/SymbolTable.java
+++ src/comp/SymbolTable.java
@@ -1,6 +1,38 @@
 package comp;
 
+import java.util.Hashtable;
+import ast.Member;
+
 
 public class SymbolTable {
+    private Hashtable classMembers;
+    private Hashtable globalVarTable;
+    private Hashtable functions;
+    private Hashtable classes;
 
+
+    //builder
+    public SymbolTable(){
+
+        /* About "members":
+        * The name of a method or classfield (instance variable) should be different
+        * from the name of the previous members of the class and
+        * different from the class name.
+        * */
+
+        this.classMembers = new Hashtable();
+        this.globalVarTable = new Hashtable();
+        this.classes = new Hashtable();
+    }
+
+    //limpa tabela de membros
+    public void resetClassMembers(){
+        this.classMembers.clear();
+    }
+
+    //Insere novos membros da classe
+    public void insertClassMember(String key, Member member){
+        this.classMembers.put(key, member);
+    }
+    //continue ...
 }
